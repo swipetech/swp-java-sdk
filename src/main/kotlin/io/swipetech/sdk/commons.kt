32594a5478273197183c183faf1be9be4b08c197
kalil reis de sisto <kalil_reis@hotmail.com>
2019-03-15 12:01:09 -0300

    gson dep removed

--- src/main/kotlin/io/swipetech/sdk/commons.kt
+++ src/main/kotlin/io/swipetech/sdk/commons.kt
@@ -2,10 +2,6 @@ package io.swipetech.sdk
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
-import com.google.common.reflect.TypeToken
-import com.google.gson.FieldNamingPolicy
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
 import io.swipetech.commons.dtos.ErrorDTO
 import khttp.responses.Response
 
@@ -17,16 +13,17 @@ fun checkError(resp: Response) {
             throw Exception(resp.text)
         }
 
-        val errorDTO = gson.fromJson(resp.jsonObject["error"].toString(), ErrorDTO::class.java)
+        val errorDTO =  jacksonObjectMapper().readValue<ErrorDTO>(resp.jsonObject["error"].toString())
+
         errorDTO?.let {
             throw it
         }
     }
 }
 
-inline fun  <reified  T:Any> getResp(resp: Response, debug: Boolean = false): T {
+inline fun <reified T : Any> getResp(resp: Response, debug: Boolean = false): T {
 
-    val response =   jacksonObjectMapper().readValue<T>(resp.jsonObject.toString())
+    val response = jacksonObjectMapper().readValue<T>(resp.jsonObject.toString())
 
     if (debug) {
         println("Got response:")
@@ -36,10 +33,4 @@ inline fun  <reified  T:Any> getResp(resp: Response, debug: Boolean = false): T 
 
     return response
 }
-
-//FIXME change gson for jackson
-val gson: Gson = GsonBuilder()
-    .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-    .create()
-
 
