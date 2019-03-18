448f5299324f05196e1a822cc76ad48789e466c2
kalil reis de sisto <kalil_reis@hotmail.com>
2019-03-18 13:53:39 -0300

    localizedMessage added

--- src/main/kotlin/io/swipetech/commons/dtos/dtos.kt
+++ src/main/kotlin/io/swipetech/commons/dtos/dtos.kt
@@ -159,7 +159,7 @@ data class FilterDTO(
         val tag: String? = null
 )
 
-@JsonIgnoreProperties("message", "cause", "stackTrace", "suppressed")
+@JsonIgnoreProperties("message", "cause", "stackTrace", "suppressed", "localizedMessage")
 data class ErrorDTO(val code: String,
                     val msg: String,
                     @JsonProperty("sub_errors")
