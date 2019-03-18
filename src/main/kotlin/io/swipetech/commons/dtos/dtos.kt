1c09c704ebba0ba448125e75b50b0ba0cfe10e79
kalil reis de sisto <kalil_reis@hotmail.com>
2019-03-18 13:43:27 -0300

    stackTrace added to JsonIgnoreProperties

--- src/main/kotlin/io/swipetech/commons/dtos/dtos.kt
+++ src/main/kotlin/io/swipetech/commons/dtos/dtos.kt
@@ -159,7 +159,7 @@ data class FilterDTO(
         val tag: String? = null
 )
 
-@JsonIgnoreProperties("message", "cause")
+@JsonIgnoreProperties("message", "cause", "stackTrace")
 data class ErrorDTO(val code: String,
                     val msg: String,
                     @JsonProperty("sub_errors")
