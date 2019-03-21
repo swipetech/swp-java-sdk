e718ccf57374c7f6a557bf78758d77ca139ddded
ezequiel <ezequiel454@gmail.com>
2019-03-21 16:21:02 -0300

    change dto account to accept accountFields

--- src/main/kotlin/io/swipetech/commons/dtos/dtos.kt
+++ src/main/kotlin/io/swipetech/commons/dtos/dtos.kt
@@ -72,7 +72,8 @@ data class OrgDTO(
 data class AccountDTO(
     override val id: String,
     val balances: List<BalanceDTO> = listOf(),
-    val tags: List<String>? = listOf()
+    val tags: List<String>? = listOf(),
+    val fieldsAccount: String? = null
 ) : IActionDTO {
     companion object
 }
@@ -84,7 +85,10 @@ data class NewAccDTO(
 
     @get:Size(max = 10, message = "{tag_invalid_size}")
         @JsonProperty("tags")
-        val tags: List<String>? = listOf()
+        val tags: List<String>? = listOf(),
+
+    @JsonProperty("fields_account")
+        val fieldsAccount: String? = null
 ) : INewActionDTO
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NONE)
