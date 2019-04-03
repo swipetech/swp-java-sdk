9971c25ccb062fae74c4cbf5cbea97f361f0de02
Eduardo Nuzzi <eduardomnuzzi@gmail.com>
2019-04-03 11:08:49 -0300

    updated signature algorithm

--- src/main/kotlin/io/swipetech/commons/actions/actions.kt
+++ src/main/kotlin/io/swipetech/commons/actions/actions.kt
@@ -5,12 +5,12 @@ import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
 
 fun sign(secret: String, path: String, body: String, timestamp: String) =
-    getHmac384(secret, timestamp + path + body)
+    getHmac256(secret, timestamp + path + body)
 
-fun getHmac384(key: String, message: String): String {
+fun getHmac256(key: String, message: String): String {
 
-    val hasher = Mac.getInstance("HmacSHA384")
-    hasher.init(SecretKeySpec(key.toByteArray(), "HmacSHA384"))
+    val hasher = Mac.getInstance("HmacSHA256")
+    hasher.init(SecretKeySpec(key.toByteArray(), "HmacSHA256"))
     val hash = hasher.doFinal(message.toByteArray())
     return Base64.getEncoder().encodeToString(hash)
 }
