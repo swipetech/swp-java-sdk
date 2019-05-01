668439937a19f1ecb0e3e05f5b9840e7e0fa9bc8
kalil reis de sisto <kalil_reis@hotmail.com>
2019-05-01 13:22:13 -0300

    small fix

--- src/main/java/io/swipetech/sdk/Test.java
+++ src/main/java/io/swipetech/sdk/Test.java
@@ -10,48 +10,53 @@ public class Test {
 
     public static void main(String[] args) {
 
-        String host = "http://localhost:8080";
+        try {
 
-        Swipe swp = new SwpBuilder()
-                .setApiKey(API_KEY)
-                .setSecret(SECRET)
-                .setHost(host)
-                .build();
+            String host = "http://localhost:8080";
 
-        AssetDTO assetDTO = swp.issueAsset(
-                new NewAssetDTO("Test222", "100", null)
-        ).getData().getValue();
+            Swipe swp = new SwpBuilder()
+                    .setApiKey(API_KEY)
+                    .setSecret(SECRET)
+                    .setHost(host)
+                    .build();
 
-        AccountDTO accDTO = swp.createAccount(
-                new CreateAccountBuilder()
-                        .addStartingBalance(assetDTO.getId(), "10")
-                        .addTag("tag10")
-                        .build()
-        ).getData().getValue();
+            AssetDTO assetDTO = swp.issueAsset(
+                    new NewAssetDTO("Test222", "100", null)
+            ).getData().getValue();
 
-        swp.makeTransfers(
-                new TransferBatchBuilder()
-                        .setMemo(new Memo(MemoType.TEXT.name(), "memo"))
-                        .addTransfer(ORG_ID, accDTO.getId(), assetDTO.getId(), "0.1")
-                        .addTransfer(ORG_ID, accDTO.getId(), assetDTO.getId(), "0.1")
-                        .build()
-        ).getData().getValue();
+            AccountDTO accDTO = swp.createAccount(
+                    new CreateAccountBuilder()
+                            .addStartingBalance(assetDTO.getId(), "10")
+                            .addTag("tag10")
+                            .build()
+            ).getData().getValue();
 
-        ActionBatchDTO result = swp.makeActionBatch(
-                new ActionBatchBuilder()
-                        .addIssueAsset("Test290", "100", null)
-                        .addTransfer(ORG_ID, accDTO.getId(), assetDTO.getId(), "0.1")
-                        .addCreateAccount(
-                                new CreateAccountBuilder()
-                                        .addStartingBalance(assetDTO.getId(), "10")
-                                        .addTag("tag10")
-                                        .build()
-                        )
-                        .build()
-        ).getData().getValue();
+            swp.makeTransfers(
+                    new TransferBatchBuilder()
+                            .setMemo(new Memo(MemoType.TEXT.name(), "memo"))
+                            .addTransfer(ORG_ID, accDTO.getId(), assetDTO.getId(), "0.1")
+                            .addTransfer(ORG_ID, accDTO.getId(), assetDTO.getId(), "0.1")
+                            .build()
+            ).getData().getValue();
 
-        System.out.println(result);
+            ActionBatchDTO result = swp.makeActionBatch(
+                    new ActionBatchBuilder()
+                            .addIssueAsset("Test290", "100", null)
+                            .addTransfer(ORG_ID, accDTO.getId(), assetDTO.getId(), "0.1")
+                            .addCreateAccount(
+                                    new CreateAccountBuilder()
+                                            .addStartingBalance(assetDTO.getId(), "10")
+                                            .addTag("tag10")
+                                            .build()
+                            )
+                            .build()
+            ).getData().getValue();
 
+            System.out.println(result);
+
+        } catch (ErrorDTO e) {
+
+        }
     }
 
 }
