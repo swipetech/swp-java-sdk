b40b1c8a6d7a41f6a58d2ba491e6c1942dee0b97
kalil reis de sisto <kalil_reis@hotmail.com>
2019-05-07 16:41:58 -0300

    transfer dto validated

--- src/main/kotlin/io/swipetech/commons/dtos/transfer.kt
+++ src/main/kotlin/io/swipetech/commons/dtos/transfer.kt
@@ -2,21 +2,32 @@ package io.swipetech.commons.dtos
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonTypeInfo
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NONE)
 data class NewTransferDTO(
+
     @JsonProperty("from")
+    @get:NotBlank(message = "{transfer_from_is_empty}")
+    @get:NotNull(message = "{transfer_from_is_empty}")
     val from: String,
 
+    @get:NotBlank(message = "{transfer_to_is_empty}")
+    @get:NotNull(message = "{transfer_to_is_empty}")
     @JsonProperty("to")
     val to: String,
 
+    @get:NotNull(message = "{transfer_invalid_amount}")
     @JsonProperty("amount")
     val amount: String,
 
     @JsonProperty("asset")
+    @get:NotBlank(message = "{transfer_asset_id_is_empty}")
+    @get:NotNull(message = "{transfer_asset_id_is_empty}")
     val asset: String
+
 ) : INewActionDTO
 
 @JsonTypeInfo(use = JsonTypeInfo.Id.NONE)
